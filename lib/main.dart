860c4c0774990d6bc39a69dfeef9a6d48855c42e
unknown <santoshadhikary>
2023-10-05 23:27:41 -0700

    dino game

---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:dino_game/vision_detector_views/pose_detector_view.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+import 'package:flame/flame.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'models/settings.dart';
+import 'models/player_data.dart';
+
+import 'package:camera/camera.dart';
+
+import 'package:flame/game.dart';
+import 'widgets/hud.dart';
+import 'game/dino_run.dart';
+import 'widgets/main_menu.dart';
+import 'widgets/pause_menu.dart';
+import 'widgets/settings_menu.dart';
+import 'widgets/game_over_menu.dart';
+
+List<CameraDescription> cameras = [];
+final Changer changer = Changer();
+DinoRun _dinoRun = DinoRun();
+
+Future<void> main() async {
+  // Ensures that all bindings are initialized
+  // before was start calling hive and flame code
+  // dealing with platform channels.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Makes the game full screen and landscape only.
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+  cameras = await availableCameras();
+
+  // Initializes hive and register the adapters.
+  await initHive();
+  runApp(const JustStyle());
+}
+
+// The main widget for this game.
+class DinoRunApp extends StatelessWidget {
+  const DinoRunApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dino Run',
+      theme: ThemeData(
+        fontFamily: 'Audiowide',
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        // Settings up some default theme for elevated buttons.
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            fixedSize: const Size(200, 60),
+          ),
+        ),
+      ),
+      home: Scaffold(
+        body: GameWidget(
+          // This will dislpay a loading bar until [DinoRun] completes
+          // its onLoad method.
+          loadingBuilder: (conetxt) => const Center(
+            child: SizedBox(
+              width: 200,
+              child: LinearProgressIndicator(),
+            ),
+          ),
+          // Register all the overlays that will be used by this game.
+          overlayBuilderMap: {
+            MainMenu.id: (_, DinoRun gameRef) => MainMenu(gameRef),
+            PauseMenu.id: (_, DinoRun gameRef) => PauseMenu(gameRef),
+            Hud.id: (_, DinoRun gameRef) => Hud(gameRef),
+            GameOverMenu.id: (_, DinoRun gameRef) => GameOverMenu(gameRef),
+            SettingsMenu.id: (_, DinoRun gameRef) => SettingsMenu(gameRef),
+          },
+          // By default MainMenu overlay will be active.
+          initialActiveOverlays: const [MainMenu.id],
+          game: _dinoRun,
+        ),
+      ),
+    );
+  }
+}
+
+/*class JustStyle extends StatelessWidget {
+  const JustStyle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Row(
+        children: const [
+          SizedBox(
+            width: 300,
+            child: PoseDetectorView(),
+          ),
+          SizedBox(
+            width: 500,
+            child: DinoRunApp(),
+          ),
+        ],
+      )),
+    );
+  }
+} 
+class JustStyle extends StatelessWidget {
+  const JustStyle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 2,
+            child: PoseDetectorView(),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 2,
+            child: DinoRunApp(),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
+
+class JustStyle extends StatelessWidget {
+  const JustStyle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Row(
+          children: [
+            SizedBox(
+              width: 300,
+              child: PoseDetectorView(),
+            ),
+            Expanded(child: DinoRunApp()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// This function will initilize hive with apps documents directory.
+// Additionally it will also register all the hive adapters.
+Future<void> initHive() async {
+  // For web hive does not need to be initialized.
+  if (!kIsWeb) {
+    final dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+  }
+
+  Hive.registerAdapter<PlayerData>(PlayerDataAdapter());
+  Hive.registerAdapter<Settings>(SettingsAdapter());
+}
+
+// The main widget for this game.
+
+class Changer extends ChangeNotifier {
+  int selectedOpt = 0;
+  bool positionCapture = false;
+  double poseStanding = 0;
+
+  void notify() {
+    notifyListeners();
+  }
+}
---
+++ lib/vision_detector_views/pose_detector_view.dart
@@ -0,0 +1,174 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:math';
+
+import 'package:dino_game/main.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+
+import 'camera_view.dart';
+//import 'painters/pose_painter.dart';
+
+class PoseDetectorView extends StatefulWidget {
+  const PoseDetectorView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _PoseDetectorViewState();
+}
+
+class _PoseDetectorViewState extends State<PoseDetectorView> {
+  int i = 1;
+  double standing = 0;
+  int squatno = 0;
+  int frame = 0;
+  bool fixedAngle = false;
+  bool squat = false;
+
+  final PoseDetector _poseDetector =
+      PoseDetector(options: PoseDetectorOptions());
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+
+  @override
+  void dispose() async {
+    _canProcess = false;
+    _poseDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraView(
+      title: 'Pose Detector',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
+    );
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    i++;
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final poses = await _poseDetector.processImage(inputImage);
+
+    // printing left eye y coordinate
+    for (Pose pose in poses) {
+      // to access specific landmarks  [ we need hip,knee,ankle]
+      final rightHipPoint = pose.landmarks[PoseLandmarkType.rightHip];
+      final rightKneePoint = pose.landmarks[PoseLandmarkType.rightKnee];
+      final rightAnklePoint = pose.landmarks[PoseLandmarkType.rightAnkle];
+
+      if (rightHipPoint != null &&
+          rightKneePoint != null &&
+          rightAnklePoint != null) {
+        // slope of hip and knee
+        double slopeA = (rightHipPoint.y - rightKneePoint.y) /
+            (rightHipPoint.x - rightKneePoint.x);
+
+        double slopeB = (rightAnklePoint.y - rightKneePoint.y) /
+            (rightAnklePoint.x - rightKneePoint.x);
+
+        double angle = atan((slopeB - slopeA) / (1 + slopeA * slopeB));
+
+        // convert angle to radian
+
+        double degree = angle * 180 / pi;
+        changer.selectedOpt == 0;
+        changer.notify();
+
+        // Store average value of standing of between 50 and 100 frames
+
+        if (!changer.positionCapture) {
+          frame++;
+          debugPrint("frame no :$frame");
+        }
+
+        if (frame >= 50 && frame <= 100) {
+          standing += degree;
+        }
+
+        if (frame > 100 && !changer.positionCapture) {
+          changer.positionCapture = true;
+          standing = standing / 50;
+          debugPrint("The average degree of standing is ${standing.abs()}");
+          const snackBar = SnackBar(
+            content: Text('you are ready to take squat'),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        }
+
+        // check squat
+
+        if (changer.positionCapture && !squat) {
+          debugPrint("CURRENT ANGLE: ${degree.abs()}");
+          if ((degree.abs() - standing.abs() > 40)) {
+            squat = true;
+            squatno++;
+            changer.selectedOpt = 1;
+            changer.notify();
+
+            // print("SQUAT");
+          } else {
+            changer.selectedOpt = 0;
+            changer.notify();
+          }
+        }
+
+        // check for standing
+
+        if (changer.positionCapture && squat) {
+          debugPrint("CURRENT ANGLE: ${degree.abs()}");
+          if ((degree.abs() - changer.poseStanding.abs() < 20)) {
+            squat = false;
+            // print("STAND");
+          }
+        }
+
+        debugPrint("No of squats : $squatno");
+      }
+    }
+
+    //##
+    // print(leftEyeY);
+    // changer.selectedOpt == 0;
+    // changer.notify();
+
+    // if (!up) {
+    //   if (leftEyeY != null) {
+    //     if (leftEyeY > 500) {
+    //       up = true;
+    //       changer.selectedOpt = 1;
+    //       changer.notify();
+    //     } else {
+    //       changer.selectedOpt = 0;
+    //       changer.notify();
+    //     }
+    //   }
+    // }
+
+//     if (inputImage.inputImage?.size != null &&
+//         inputImage.inputImage?.imageRotation != null) {
+//       final painter = PosePainter(poses, inputImage.inputImage!.size,
+//           inputImage.inputImage!.imageRotation);
+//       _customPaint = CustomPaint(painter: painter);
+//     } else {
+//       _text = 'Poses found: ${poses.length}\n\n';
+//       _customPaint = null;
+//     }
+//     _isBusy = false;
+//     if (mounted) {
+//       setState(() {});
+//     }
+//   }
+// }
+  }
+}
